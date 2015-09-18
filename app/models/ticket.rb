bb531d0dc1f70b45dbb03d418b8dee62c1263492
Whitney Hiemstra <whitcolorado@gmail.com>
2015-09-18 12:12:11 -0600

    Ticket validations set up, specs passing

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ActiveRecord::Base
   belongs_to :board
 
+  validates_presence_of :name
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:ticket) { Ticket.create(name: "Ticket title", description: "Ticket description", status: 0) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it "is invalid without a title" do
+    ticket.name = ""
+    expect(ticket).to_not be_valid
+  end
+
+  it "is valid without a description, description can be blank" do
+    ticket.description = ""
+    expect(ticket).to be_valid
+  end
+
+  it "is valid without a status; defaults to backlog" do
+    #need to test enum... backlog
+
+    ticket_2 = Ticket.create(name: "Ticket title #2")
+    expect(ticket_2).to be_valid
+    expect(ticket_2.status).to eq(0)
+  end
+
+
+
+
 end
